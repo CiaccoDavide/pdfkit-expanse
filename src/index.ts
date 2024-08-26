66fc9b44ddf440e1f8c24f41894f0a9f5a4abdb1
Davide Ciacco <ciaccodavide@gmail.com>
2024-08-26 14:53:13 +0200

    updated cheerio and added optional headers/footers to pdf pages

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,14 @@ export enum SupportedFonts {
   HelveticaBold = "Helvetica-Bold",
 }
 
-export const PAGE_WIDTH = 595;
+export interface ChartProps {
+  chartConfiguration: ChartConfiguration;
+  table?: Table;
+  width?: number;
+  height?: number;
+}
+
+export const PAGE_WIDTH = 595; // total page width, margins included
 export const PAGE_HEIGHT = PAGE_WIDTH * 1.414;
 export const PAGE_MARGIN = 72;
 export const CONTENT_WIDTH = PAGE_WIDTH - PAGE_MARGIN * 2;
@@ -56,11 +63,32 @@ export type PdfGenerationSection = {
   options?: any;
 };
 
+// export interface TwoColumnsContent {
+//   left?: PdfGenerationSection[] | "pageIndicator";
+//   right?: PdfGenerationSection[] | "pageIndicator";
+// }
+
+// export interface ThreeColumnsContent extends TwoColumnsContent {
+//   center?: PdfGenerationSection[] | "pageIndicator";
+// }
+
+export interface Header {
+  logo?: string;
+}
+
+export interface Footer {
+  title?: string;
+  subtitle?: string;
+  showPageNumbers?: boolean;
+}
+
 export interface PdfGenerationInput {
   textColor?: string;
   primaryColor?: string;
   secondaryColor?: string;
   sections: PdfGenerationSection[];
+  header?: Header;
+  footer?: Footer;
 }
 
 export interface PdfGeneratorGlobalSettings {
@@ -83,6 +111,9 @@ export default class PdfGenerator {
   primaryColor: string = "#000000";
   secondaryColor: string = "#000000";
 
+  header?: Header;
+  footer?: Footer;
+
   private htmlStyles = (
     textColor: string,
     primaryColor: string,
@@ -95,7 +126,7 @@ export default class PdfGenerator {
     defaultFontFamilyBold = SupportedFonts.HelveticaBold,
     customDirname = __dirname,
   }: PdfGeneratorGlobalSettings = {}) {
-    this.doc = new PDFDocumentWithTables({ size: "A4" });
+    this.doc = new PDFDocumentWithTables({ size: "A4", bufferPages: true });
 
     this.firefoxCss = fs.readFileSync(
       path.join(customDirname, "../assets/firefox-html.css"),
@@ -256,22 +287,29 @@ export default class PdfGenerator {
     await this.addHtmlContent(html);
   }
 
-  private async drawChart(configuration: ChartConfiguration, table?: Table) {
+  private async drawChart({
+    chartConfiguration,
+    table,
+    width,
+    height,
+  }: ChartProps) {
     // let x = this.doc.x;
     let y = this.doc.y;
 
-    const chartWidth = 150;
-    const chartHeight = 150;
+    const chartWidth = table ? 150 : width || 150;
+    const chartHeight = table ? 150 : height || 150;
     const renderedChartWidth = CONTENT_WIDTH / 3;
     const renderedChartHeight = (chartHeight * renderedChartWidth) / chartWidth;
 
     // If the chart or the table does not fit on the page, add a new page
     const isChartOverflowing =
       y + renderedChartHeight > CONTENT_HEIGHT + PAGE_MARGIN;
+    const tableHeight = table
+      ? 60 + (table.rows?.length || 0 || table.datas?.length || 0) * 10
+      : 0;
     const isTableOverflowing =
       table && // if there is no table, it can't overflow
-      this.doc.y + 60 + (table.rows?.length || table.datas?.length || 0) * 10 >
-        CONTENT_HEIGHT;
+      this.doc.y + tableHeight + 60 > CONTENT_HEIGHT;
 
     if (isChartOverflowing || isTableOverflowing) {
       this.doc.addPage({ size: "A4" });
@@ -287,10 +325,17 @@ export default class PdfGenerator {
     }
 
     // move pdfkit cursor to the bottom of the chart
-    this.doc.fontSize(renderedChartHeight / (table ? 1.2 : 1.4));
-    this.doc.moveDown();
+    const chartHeightSpaced = chartHeight * (table ? 1.3 : 1.5);
+    this.doc.fontSize(chartHeightSpaced);
     if (!table) this.doc.text(" ");
-
+    else {
+      if (chartHeightSpaced > tableHeight) {
+        this.doc.fontSize(chartHeightSpaced - tableHeight);
+        this.doc.text(" ");
+      } else {
+        this.doc.moveDown();
+      }
+    }
     // Render the chart to an SVG with chartjs-node-canvas
     const chartJSNodeCanvas = new ChartJSNodeCanvas({
       type: "svg",
@@ -299,7 +344,7 @@ export default class PdfGenerator {
     });
 
     const imageBuffer = chartJSNodeCanvas.renderToBufferSync({
-      ...configuration,
+      ...chartConfiguration,
       plugins: {
         legend: {
           display: true,
@@ -311,8 +356,11 @@ export default class PdfGenerator {
       .toString("utf8")
       .replace(`width="${chartWidth * 2}pt" height="${chartHeight * 2}pt"`, "");
 
+    const centeredPosition =
+      PAGE_MARGIN + CONTENT_WIDTH / 2 - chartWidth / 1.333;
+
     // Insert the SVG into the PDF
-    SVGtoPDF(this.doc, svg, 320 - (table ? 0 : 135), y, {
+    SVGtoPDF(this.doc, svg, table ? 320 : centeredPosition, y, {
       width: renderedChartWidth * 2,
       height: renderedChartHeight * 2,
     });
@@ -332,11 +380,16 @@ export default class PdfGenerator {
     primaryColor,
     secondaryColor,
     sections,
+    header,
+    footer,
   }: PdfGenerationInput): Promise<PassThrough> {
     this.resetFont(); // init font
     this.textColor = textColor ?? "#000000";
     this.primaryColor = primaryColor ?? "#13937f";
     this.secondaryColor = secondaryColor ?? "#a27222";
+
+    this.header = header;
+    this.footer = footer;
 
     for (const section of sections) {
       this.doc.fillColor(this.textColor);
@@ -360,6 +413,9 @@ export default class PdfGenerator {
           this.doc.fontSize(options.fontSize ?? 14);
           this.doc.text(options.content, x, y, {
             align: options.align ?? "left",
+            ...options.width ? { width: options.width } : {},
+            ...options.height ? { height: options.height } : {},
+            ...options.lineGap ? { lineGap: options.lineGap } : {},
           });
 
           if (options.x && options.y) {
@@ -375,7 +431,7 @@ export default class PdfGenerator {
           await this.addHtmlContent(options.content);
           break;
         case PdfGenerationSectionType.CHART:
-          await this.drawChart(options.chartConfiguration, options.table);
+          await this.drawChart(options);
           break;
         case PdfGenerationSectionType.TABLE:
           // If the table does not fit on the page, add a new page
@@ -443,8 +499,93 @@ export default class PdfGenerator {
       this.resetFont();
     }
 
+    this.renderHeaderAndFooter();
+
     this.doc.end();
     return this.stream;
+  }
+
+  private renderHeaderAndFooter() {
+    const footerMargin = 16;
+    const range = this.doc.bufferedPageRange();
+
+    for (let i = range.start; i < range.start + range.count; i++) {
+      this.doc.switchToPage(i);
+
+      // header
+      if (this.header && this.header.logo) {
+        const headerBoxX = footerMargin;
+        const headerBoxY = footerMargin;
+        const headerBoxWidth = this.doc.page.width - footerMargin * 2;
+        const headerBoxHeight = PAGE_MARGIN - footerMargin * 3;
+        this.doc.image(this.header.logo, headerBoxX, headerBoxY, {
+          align: "right",
+          fit: [headerBoxWidth, headerBoxHeight],
+        });
+      }
+
+      // footer
+      if (this.footer) {
+        this.doc.strokeColor("#eeeeee");
+        this.doc
+          .moveTo(footerMargin, PAGE_HEIGHT - PAGE_MARGIN + footerMargin)
+          .lineTo(
+            PAGE_WIDTH - footerMargin,
+            PAGE_HEIGHT - PAGE_MARGIN + footerMargin
+          )
+          .stroke();
+
+        this.doc.strokeColor(this.textColor);
+
+        this.doc.fillColor("#728197");
+        const footerFontSize = 8.5;
+        const footerLineGap = 4;
+
+        const footerTextBoxY =
+          this.doc.page.height - PAGE_MARGIN + footerMargin * 2;
+        const footerTextBoxHeight = footerFontSize;
+
+        this.doc.fontSize(footerFontSize);
+
+        if (this.footer?.title) {
+          this.doc.font(this.FONT_BOLD);
+          this.doc.text(this.footer.title, footerMargin, footerTextBoxY, {
+            width: this.doc.page.width - footerMargin * 2,
+            height: footerTextBoxHeight,
+            align: "left",
+            lineGap: 2,
+          });
+          this.doc.font(this.FONT_DEFAULT);
+        }
+        if (this.footer?.subtitle) {
+          this.doc.text(
+            this.footer.subtitle,
+            footerMargin,
+            footerTextBoxY + footerFontSize + footerLineGap,
+            {
+              width: this.doc.page.width - footerMargin * 2,
+              height: footerTextBoxHeight,
+              align: "left",
+              lineGap: 2,
+            }
+          );
+        }
+
+        if (this.footer?.showPageNumbers) {
+          this.doc.text(
+            `${i + 1}/${range.count}`,
+            footerMargin,
+            footerTextBoxY + footerFontSize + footerLineGap,
+            {
+              width: this.doc.page.width - footerMargin * 2,
+              height: footerTextBoxHeight,
+              align: "right",
+              lineGap: 2,
+            }
+          );
+        }
+      }
+    }
   }
 
   static async generateReport(
